18e854594c734e1f54388500dffe39f57dc3c9c7
Stalhammar <162510466+Stalhammar@users.noreply.github.com>
2025-10-16 21:50:59 +0200

    style: Update App, Index and SideBar CSS

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import Experience from "./pages/Experience";
 function App() {
   return (
     <Router>
-      <div className="flex">
+      <div className="min-h-screen bg-gray-100">
         <SideBar />
-        <main>
+        <main className="ml-48 p-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/experience" element={<Experience />} />
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,8 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col">
-      <nav className="flex flex-col">
+    <aside
+      className="
+        fixed left-6 top-6 h-[calc(100vh-3rem)] w-32
+        bg-white shadow-xl rounded-2xl
+        flex flex-col items-start justify-start p-6
+    "
+    >
+      <nav className="flex flex-col gap-4 text-gray-700 font-medium">
         <NavLink
           to="/"
           end
